7c50f729d63ce15e37447b63ab74d84f4df681f3
Vladimir RM <shvdrummer@gmail.com>
2023-02-20 21:28:32 +0200

    add h1 flag for register

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-
-import './App.css';
-import ConfirmDialog from './ConfirmDialog';
-import RegisterForm from './RegisterForm';
+import "./App.css";
+import ConfirmDialog from "./ConfirmDialog";
+import RegisterForm from "./RegisterForm";
 
 function App() {
   return (
     <div className="App">
       <main>
-        <RegisterForm/>
-        <ConfirmDialog/>
+        <RegisterForm />
+        <ConfirmDialog />
       </main>
-
-
-      
-   
     </div>
-  
   );
-  function RegisterForm (){
-    return (
-    <div>
-      
-    </div>
-
-    )
+  function RegisterForm() {
+    return <div>
+      <h1>Please, register</h1>
+    </div>;
   }
 }
 
