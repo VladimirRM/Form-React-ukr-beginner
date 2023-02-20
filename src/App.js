03d90eaeea2e8fe483afd91fb9752edbaf64f8d6
Vladimir RM <shvdrummer@gmail.com>
2023-02-20 21:27:33 +0200

    add Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 
 import './App.css';
+import ConfirmDialog from './ConfirmDialog';
+import RegisterForm from './RegisterForm';
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <RegisterForm/>
+        <ConfirmDialog/>
+      </main>
+
+
+      
    
     </div>
+  
   );
+  function RegisterForm (){
+    return (
+    <div>
+      
+    </div>
+
+    )
+  }
 }
 
 export default App;
