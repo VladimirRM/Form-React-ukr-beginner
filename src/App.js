d6125042df1e41a3a7551dbf5c2ecb16743dc973
Vladimir RM <shvdrummer@gmail.com>
2023-02-21 08:16:27 +0200

    change type submit

--- src/App.js
+++ src/App.js
@@ -7,14 +7,33 @@ function App() {
     <div className="App">
       <main>
         <RegisterForm />
-        <ConfirmDialog />
       </main>
+      <ConfirmDialog />
     </div>
   );
   function RegisterForm() {
-    return <div>
-      <h1>Please, register</h1>
-    </div>;
+    return (
+      <div>
+        <h1>Please, register</h1>
+        <form onSubmit={console.log("done")}>
+          <input
+            name="email"
+            label="label"
+            type="email"
+            onChange={console.log()}
+            required={true}
+          />
+          <input
+            name="pwd"
+            label="password"
+            type="password"
+            onChange={console.log()}
+            required={true}
+          />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
   }
 }
 
